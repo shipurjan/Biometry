79280d98e826e679a06f1b2d7fcf2b20d607275c
Bartłomiej Lindert <b.lindert@hotmail.com>
2016-06-14 18:55:10 +0200

    ef

--- Fingerprints/Fingerprints/Models/SelfDefinedMinutiae.cs
+++ Fingerprints/Fingerprints/Models/SelfDefinedMinutiae.cs
@@ -5,13 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity.Core;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fingerprints
 {
     class SelfDefinedMinutiae
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int MinutiaeId { get; set; }
         public string Name { get; set; }
         public int ProjectId { get; set; }
