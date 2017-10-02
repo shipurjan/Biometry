e1f14e10b1cef98b26a0160e8c0135a3a3592c64
Bartłomiej Lindert <xray496@gmail.com>
2017-10-02 12:06:25 +0200

    Point drawing

--- Fingerprints/Fingerprints/Factories/MinutiaStateFactory.cs
+++ Fingerprints/Fingerprints/Factories/MinutiaStateFactory.cs
@@ -21,6 +21,10 @@ namespace Fingerprints.Factories
             {
                 switch (_oMinutia.TypeId)
                 {
+                    case 1:
+                        oMinutiaState = new PointState(_oDrawingService);
+                        oMinutiaState.Minutia = _oMinutia;
+                        break;
                     case 2:
                         oMinutiaState = new VectorState(_oDrawingService);
                         oMinutiaState.Minutia = _oMinutia;
---
+++ Fingerprints/Fingerprints/MinutiaeTypes/PointState.cs
@@ -0,0 +1,54 @@
+﻿using Fingerprints.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Fingerprints.ViewModels;
+using ExceptionLogger;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using Fingerprints.Resources;
+
+namespace Fingerprints.MinutiaeTypes
+{
+    class PointState : MinutiaStateBase, IMouseClickable, IDrawable
+    {
+        public PointState(DrawingService _oDrawingService) : base(_oDrawingService)
+        {
+        }
+
+        public void DrawProcedure()
+        {
+            try
+            {
+                if (Points.Count == 1)
+                {
+                    // Get beging and end of drawing
+                    var firstPoint = Points[0];
+
+                    // Draw elipse starting in firstPoint location and fixed radius size
+                    WriteableBmp.DrawEllipseCentered(Convert.ToInt16(firstPoint.X), Convert.ToInt16(firstPoint.Y), 5, 5, Colors.Green);                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteExceptionLog(ex);
+            }
+        }
+
+        public void MouseClick(object sender, MouseButtonEventArgs args)
+        {
+            if (args.RightButton == MouseButtonState.Pressed)
+            {
+                if (Points.Count == 0)
+                {                   
+                    Points.Add(args.GetPosition((IInputElement)sender).ToFloorPoint());
+                    DrawingService.InitiateNewDrawing();
+                }
+            }
+        }
+    }
+}
