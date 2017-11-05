143a567598c3e85caf396ad6de21ff29d4cb961c
Mateusz Wróblewski <wrobmat@gmail.com>
2017-11-05 12:10:16 +0100

    disable delete button

--- Fingerprints/Fingerprints/ViewModels/GridViewModel.cs
+++ Fingerprints/Fingerprints/ViewModels/GridViewModel.cs
@@ -19,5 +19,26 @@ namespace Fingerprints.ViewModels
 
         private MinutiaStateBase rightDrawingObject;
         public MinutiaStateBase RightDrawingObject { get { return rightDrawingObject; } set { SetProperty(ref rightDrawingObject, value); } }
+
+        private bool deleteButtonVisible;
+
+        public bool DeleteButtonVisible
+        {
+            get { return deleteButtonVisible; }
+            set { SetProperty(ref deleteButtonVisible, value); }
+        }
+
+        public GridViewModel()
+        {
+            try
+            {
+                deleteButtonVisible = true;
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteExceptionLog(ex);
+            }
+        }
+
     }
 }
