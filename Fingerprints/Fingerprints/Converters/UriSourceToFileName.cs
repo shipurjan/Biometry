a8f626b27d4d0955bf92fa787b3f623d573978a9
Mateusz Wróblewski <wrobmat@gmail.com>
2017-11-09 13:53:17 +0100

    bottom bar in progress

---
+++ Fingerprints/Fingerprints/Converters/UriSourceToFileName.cs
@@ -0,0 +1,36 @@
+﻿using ExceptionLogger;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Fingerprints.Converters
+{
+    class UriSourceToFileName : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            string result = "";
+            try
+            {
+                Uri UriSource = (Uri)value;
+
+                result = Path.GetFileName(UriSource.AbsolutePath);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteExceptionLog(ex);
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
