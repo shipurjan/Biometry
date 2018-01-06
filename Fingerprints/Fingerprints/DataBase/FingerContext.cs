92d8cdaa4cec55c65e82596b69c7c6360bc6fe4d
Unknown <wrobmat@gmail.com>
2018-01-06 12:20:55 +0100

    delete sqlLite providers

--- Fingerprints/Fingerprints/DataBase/FingerContext.cs
+++ Fingerprints/Fingerprints/DataBase/FingerContext.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fingerprints.DataBase;
 using System.Data.Entity;
-using Fingerprints.DataBase;
 
 namespace Fingerprints
 {
